e8c8875800a958318e62d57fc3285eb13e26ebbb
P1ppinu <iacono760@gmail.com>
2022-01-27 17:55:13 +0100

    Initial commit

---
+++ funzioni.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import csv
+import re
+from csv import writer
+from bs4 import BeautifulSoup
+
+def scrapeProducts(raw_dict, clean_dict, shops):
+    for shop in shops:
+        r = requests.get(shop)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        res = soup.find_all('script', type = 'application/ld+json')[3:]
+
+        for item in res:
+            CT = item.contents[0]
+            newItem = json.loads(CT)
+            raw_dict[newItem['name']] = {key: newItem[key] for key in newItem.keys() & {'offers'}}
+
+        for k,v in raw_dict.items():
+            # print(k,v)
+            clean_dict[k] = {'prezzo': v['offers']['price'], 'supermercato': v['offers']['seller']['name']}
+
+def saveAsCSV(product_dict):
+    from csv import writer
+    with open('offerte.csv', 'a') as f_object:
+
+        writer_object = writer(f_object)
+
+        fieldnames = ['prodotto', 'prezzo', 'supermercato']
+        writer = csv.DictWriter(f_object, fieldnames=fieldnames)
+
+        for k,v in product_dict.items():
+            writer.writerow({'prodotto': k, 'prezzo': v['prezzo'], 'supermercato': v['supermercato']})
+
+        f_object.close()
+
+def saveAsJSON(product_dict):
+
+    with open('offerte.json', 'w') as f:
+        json.dump(product_dict, f)
+
+        f.close()
+
+def ricercaProdotto(product):
+
+    matched_dict = dict()
+
+    with open('offerte.json','r') as f:
+        prod_dict = json.load(f)
+
+        for k,v in prod_dict.items():
+            # print(product, k, v)
+            if re.search(product, k, re.IGNORECASE):
+                matched_dict[k] = v
+
+        f.close()
+
+    return matched_dict
---
+++ main.py
@@ -0,0 +1,17 @@
+from soupsieve import match
+from funzioni import *
+import re
+
+shops = list()
+
+with open('shops.txt', 'r') as f:
+    for word in f:
+        shops.append(word)
+
+listaProdotti = dict()
+cleanList = dict()
+
+scrapeProducts(listaProdotti, cleanList, shops)
+saveAsJSON(cleanList)
+d = ricercaProdotto('pomodor')
+print(d)
